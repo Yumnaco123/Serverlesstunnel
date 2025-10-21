eb82d04d9de2ff433c69c01b58f92e92e7f42c67
Yumnaco123 <ranimahrani830@gmail.com>
2025-10-21 23:58:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '172-235-246-55';
+let proxyIP = '172-232-238-52';
 //let sub = '';
 let subConverter = atob('U3ViQXBpLkNtbGlVc3NzUy5OZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
@@ -5816,6 +5816,7 @@ async function handleWebSocket(request) {
     });
 
 }
+
 
 
 
